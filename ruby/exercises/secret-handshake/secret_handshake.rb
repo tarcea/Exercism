ec7cff30a3ea0211dab5327ce00666f7ee98c7e7
Gheorghe Tarcea <tarcea@gmail.com>
2020-05-25 14:40:29 +0200

    trials

--- ruby/exercises/secret-handshake/secret_handshake.rb
+++ ruby/exercises/secret-handshake/secret_handshake.rb
@@ -6,3 +6,8 @@ To get started with TDD, see the `README.md` file in your
 `ruby/secret-handshake` directory.
 =end
 
+class SecretHandshake
+  def initialize(number)
+    @binar = number.to_s(2)
+  end
+end
