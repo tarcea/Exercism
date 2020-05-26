e99f11b0ea24d52402837287b110ebdfd6ae7cfd
Gheorghe Tarcea <tarcea@gmail.com>
2020-05-26 21:16:28 +0200

    resistor-color-trio done, no refacto

--- ruby/exercises/resistor-color-trio/resistor_color_trio.rb
+++ ruby/exercises/resistor-color-trio/resistor_color_trio.rb
@@ -6,3 +6,38 @@ To get started with TDD, see the `README.md` file in your
 `ruby/resistor-color-trio` directory.
 =end
 
+class ResistorColorTrio
+  # attr_reader :color1, :color2, :color3
+  def initialize(colors)
+    @color1 = colors.first
+    @color2 = colors[1]
+    @color3 = colors.last
+  end
+
+  def label
+    if calcul.to_i < 1000
+      "Resistor value: #{calcul} ohms"
+    elsif calcul.to_i >= 1000 && calcul.to_i < 1000000
+      "Resistor value: #{calcul.delete_suffix('000')} kiloohms"
+    end
+  end
+
+  private
+  # attr_reader :color1, :color2, :color3
+  def calcul
+    raise ArgumentError.new('invalid color, bai animalule...') unless valid_color
+    code.find_index(@color3) == 0 ? zero = '' : zero = '0' * code.find_index(@color3)
+    "#{code.find_index(@color1)}#{code.find_index(@color2)}#{zero}"
+  end
+
+  def valid_color
+    code.include?(@color1) && code.include?(@color2) && code.include?(@color3)
+  end
+
+  def code
+    ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+  end
+end
+
+# res = ResistorColorTrio.new(["orange", "orange", "black"])
+# p res.label
--- ruby/exercises/resistor-color-trio/resistor_color_trio_test.rb
+++ ruby/exercises/resistor-color-trio/resistor_color_trio_test.rb
@@ -9,27 +9,27 @@ class ResistorColorTrioTest < Minitest::Test
   end
 
   def test_blue_and_grey_and_brown
-    skip
+    # skip
     assert_equal "Resistor value: 680 ohms", ResistorColorTrio.new(["blue", "grey", "brown"]).label
   end
 
   def test_red_and_black_and_red
-    skip
+    # skip
     assert_equal "Resistor value: 2 kiloohms", ResistorColorTrio.new(["red", "black", "red"]).label
   end
 
   def test_green_and_brown_and_orange
-    skip
+    # skip
     assert_equal "Resistor value: 51 kiloohms", ResistorColorTrio.new(["green", "brown", "orange"]).label
   end
 
   def test_yellow_and_violet_and_yellow
-    skip
+    # skip
     assert_equal "Resistor value: 470 kiloohms", ResistorColorTrio.new(["yellow", "violet", "yellow"]).label
   end
 
   def test_invalid_color
-    skip
+    # skip
     assert_raises(ArgumentError) do
       ResistorColorTrio.new(["yellow", "purple", "black"]).label
     end
