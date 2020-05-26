3ccb0bf9b881e04549db405c4c09e61f1494e0ab
Gheorghe Tarcea <tarcea@gmail.com>
2020-05-26 15:02:17 +0200

    secret-handshake

--- ruby/exercises/secret-handshake/secret_handshake.rb
+++ ruby/exercises/secret-handshake/secret_handshake.rb
@@ -1,13 +1,44 @@
 =begin
-Write your code for the 'Secret Handshake' exercise in this file. Make the tests in
-`secret_handshake_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/secret-handshake` directory.
+1 = wink 1
+10 = double blink 2
+100 = close your eyes 4
+1000 = jump 8
+10000 = Reverse the order of the operations in the secret handshake. 16
 =end
+
 
 class SecretHandshake
   def initialize(number)
-    @binar = number.to_s(2)
+    @binar = number.to_s(2).reverse.split(//).map(&:to_i)
+
+  end
+
+  def commands
+    h = []
+    (0..3).each do |i|
+      if @binar[i] == 1
+      h << mes[i]
+      end
+    end
+    if @binar[4] == 1
+      h.reverse
+    else
+      h
+    end
+  end
+
+  def mes
+    ['wink', 'double blink', 'close your eyes', 'jump']
   end
 end
+
+a = SecretHandshake.new(31)
+b = SecretHandshake.new(19)
+c = SecretHandshake.new(9)
+d = SecretHandshake.new(7)
+e = SecretHandshake.new(5)
+p a.commands
+p b.commands
+p c.commands
+p d.commands
+p e.commands
