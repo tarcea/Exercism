34c465bdebd97d1d848a313d2b00a075f5bc3216
Gheorghe Tarcea <tarcea@gmail.com>
2020-05-28 11:36:05 +0200

    trials

--- ruby/exercises/twelve-days/twelve_days.rb
+++ ruby/exercises/twelve-days/twelve_days.rb
@@ -6,3 +6,42 @@ To get started with TDD, see the `README.md` file in your
 `ruby/twelve-days` directory.
 =end
 
+class TwelveDays
+  def self.song
+  i = 0
+  a = []
+    until i == 11 do
+      case i
+        when 0
+         a << "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\n"
+     # puts "On the #{days[i]} #{chr} #{i == 0 ? 'a' : ', and a'} #{par}"
+        when 1
+         a << "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.\n"
+        when 2
+         a << "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+        when 3
+         a << "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+        when 4
+         a << "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+        when 5
+         a << "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+        when 6
+         a << "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+        when 7
+         a << "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+        when 8
+         a << "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+        when 9
+         a << "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+        when 10
+         a << "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+        when 11
+         a << "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+      end
+      i += 1
+    end
+    a.join("\n")
+  end
+end
+
+# TwelveDays.song
