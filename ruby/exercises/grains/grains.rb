22552105df34a768234502ec03e00a31e3f9e902
Gheorghe Tarcea <tarcea@gmail.com>
2020-05-28 12:04:50 +0200

    trials

--- ruby/exercises/grains/grains.rb
+++ ruby/exercises/grains/grains.rb
@@ -5,4 +5,13 @@ Write your code for the 'Grains' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/grains` directory.
 =end
+class Grains
+  def self.square(number)
+    raise ArgumentError if number <= 0 || number > 64
+    2 ** (number - 1)
+  end
 
+  def self.total
+    (2 ** 64) - 1
+  end
+end
