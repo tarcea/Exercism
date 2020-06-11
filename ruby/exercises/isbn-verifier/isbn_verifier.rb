e3aca07ca1b379e6befae4ef1711f7adf0846c23
Gheorghe Tarcea <tarcea@gmail.com>
2020-06-11 22:06:27 +0200

    isbn verifier

--- ruby/exercises/isbn-verifier/isbn_verifier.rb
+++ ruby/exercises/isbn-verifier/isbn_verifier.rb
@@ -6,3 +6,20 @@ To get started with TDD, see the `README.md` file in your
 `ruby/isbn-verifier` directory.
 =end
 
+class IsbnVerifier
+  def self.valid?(isbn)
+    array = isbn.split(//)
+    return false if array.empty?
+    array.delete('-')
+    array[-1] == 'x' || array[-1] == 'X' ? array[-1] = '10' : array
+    return false unless array.length == 10 && array[-1][/([0-9]*)/]
+    calc = 0
+    array.each_with_index do |el, ind|
+      return false unless el[/([0-9]*)/] != ""
+      calc += (el.to_i * (ind + 1))
+    end
+    calc % 11 == 0
+  end
+end
+
+# p IsbnVerifier.valid?('3-598-21507-X')
